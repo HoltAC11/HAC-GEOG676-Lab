dd9b76f48d0cb25551d41889e0538f273b59d6e5
HoltAC11 <holtac11@tamu.edu>
2024-10-08 22:53:14 -0500

    directory

--- Labs/Lab3/lab03.py
+++ Labs/Lab3/lab03.py
@@ -2,7 +2,7 @@ import os
 import geopandas as gpd
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DATA_DIR = os.path.join(BASE_DIR, 'data')
+DATA_DIR = os.path.join(BASE_DIR, 'Lab3')
 
 class CensusTract:
     def __init__(self, geoid, population, geometry):

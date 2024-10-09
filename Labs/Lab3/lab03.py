252c6ec2b741fe203146f8801715e60bc3bda325
HoltAC11 <holtac11@tamu.edu>
2024-10-09 11:02:21 -0500

    Calculation of population density

--- Labs/Lab3/lab03.py
+++ Labs/Lab3/lab03.py
@@ -13,19 +13,20 @@ class CensusTract:
     def calculate_population_density(self):
         # calculate the population density based on geometry
         ### >>>>>>>>>>>> YOUR CODE HERE <<<<<<<<<<< ###
-        population = row['Pop']
-        area = gpd.GeoSeries(row['geometry'])
-        area1 = row['geometry'].area
-        population_density = population / area
+        pop_temp = gdf['Pop']
+        geometry_temp = gdf.to_crs(epsg=3081)
+        area_temp = geometry_temp.area / 10**6
+        population_density = pop_temp / area_temp
         return population_density
         ### <<<<<<<<<<< END OF YOUR CODE <<<<<<<<<<< ###
+
 if __name__ == "__main__":
     # read data
     file_path = os.path.join(DATA_DIR, 'data.geojson')
     # load data into GeoDataFrame
     gdf = gpd.read_file(file_path)
     # preview data
-    print(gdf.head())
+    print(gdf.head(10))
     print(gdf.columns)
     print(gdf.shape)
     print(gdf.dtypes)
@@ -33,13 +34,21 @@ if __name__ == "__main__":
     # calculate the Population Density based on geometry
     ### >>>>>>>>>>>> YOUR CODE HERE <<<<<<<<<<< ###
     # instantiate the CensusTract class
+    CT_temp = CensusTract(
+        geoid = gdf['GeoId'],
+        population = gdf['Pop'],
+        geometry = gdf['geometry']
+    )
 
     # calculate the population density for each census tract
-
+    for index,row in gdf.iterrows():
+        CT_temp
+        pop_density_temp = CT_temp.calculate_population_density()
+        gdf = gdf.assign(Pop_Den_new = pop_density_temp)
     # create a new column for the population density and name it as 'Pop_Den_new'
 
     ### <<<<<<<<<<< END OF YOUR CODE <<<<<<<<<<< ###
 
     # preview the data again
-    print(gdf.head())
+    print(gdf.head(10))
     
